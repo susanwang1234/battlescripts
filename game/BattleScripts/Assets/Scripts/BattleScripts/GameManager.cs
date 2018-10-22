58d5c03426bced9eb0c5a7d7712aadc03445e0c9
Brian Fong <brianwkfong@gmail.com>
2018-10-22 15:37:53 -0700

    player now instantitates when play is pressed

--- game/BattleScripts/Assets/Scripts/BattleScripts/GameManager.cs
+++ game/BattleScripts/Assets/Scripts/BattleScripts/GameManager.cs
@@ -16,7 +16,12 @@ namespace BattleScripts
     {
 
         #region Public Fields
+        
         public static GameManager Instance;
+
+        [Tooltip("The Prefab used for representing the player")]
+        public GameObject playerPrefab;
+
         #endregion
 
         #region Photon Callbacks
@@ -54,6 +59,23 @@ namespace BattleScripts
 
 
                 LoadArena();
+            }
+        }
+
+        #endregion
+
+        #region MonoBehaviour CallBacks
+
+        void Start()
+        {
+            if (playerPrefab == null)
+            {
+                Debug.LogError("<Color=Red><a>Missing</a></Color> playerPrefab Reference. Please set it up in GameObject 'Game Manager'",this);
+            }
+            else
+            {
+                // we're in a room. spawn a character for the local player. it gets synced by using PhotonNetwork.Instantiate
+                PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(0f,0f,0f), Quaternion.identity, 0);
             }
         }
 
