edb6367d935f73542a24f20ef8be80c4e7bcc588
Brian Fong <brianwkfong@gmail.com>
2018-10-21 23:02:45 -0700

    Added preliminaly match making and empty game scene

--- game/BattleScripts/Assets/Scripts/BattleScripts/Launcher.cs
+++ game/BattleScripts/Assets/Scripts/BattleScripts/Launcher.cs
@@ -20,6 +20,24 @@ namespace BattleScripts
         /// This client's version number. Users are separated from each other by gameVersion (which allows you to make breaking changes).
         /// </summary>
         private string gameVersion = "1";
+
+        /// <summary>
+        /// Keep track of the current process. Since connection is asynchronous and is based on several callbacks from Photon,
+        /// we need to keep track of this to properly adjust the behavior when we receive call back by Photon.
+        /// Typically this is used for the OnConnectedToMaster() callback.
+        /// </summary>
+        bool isConnecting;
+        #endregion
+
+        #region Public Fields
+        
+        [Tooltip("The Ui Panel to let the user enter name, connect and play")]
+        [SerializeField]
+        private GameObject controlPanel;
+        [Tooltip("The UI Label to inform the user that the connection is in progress")]
+        [SerializeField]
+        private GameObject progressLabel;
+        
         #endregion
 
         #region MonoBehaviour CallBacks
@@ -33,6 +51,25 @@ namespace BattleScripts
             PhotonNetwork.AutomaticallySyncScene = true;
         }
 
+        void Start()
+        {
+            ToggleControlPanel(true);
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        /// <summary>
+        /// Toggles Control Pannel on and off.
+        /// - When Off, will turn on the progress label.
+        /// - When On, will show play button and name input field 
+        /// </summary>
+        private void ToggleControlPanel(bool isStart)
+        {
+            progressLabel.SetActive(!isStart);
+            controlPanel.SetActive(isStart);
+        }
         #endregion
 
         #region Public Methods
@@ -43,6 +80,10 @@ namespace BattleScripts
         /// </summary>
         public void Connect()
         {
+            // keep track of the will to join a room, because when we come back from the game we will get a callback that we are connected, so we need to know what to do then
+            isConnecting = true;
+
+            ToggleControlPanel(false);            
             // we check if we are connected or not, we join if we are , else we initiate the connection to the server.
             if (PhotonNetwork.IsConnected)
             {
@@ -59,21 +100,23 @@ namespace BattleScripts
         #endregion
 
         #region MonoBehaviourPunCallbacks Callbacks
-
-
         public override void OnConnectedToMaster()
         {
             Debug.Log("PUN Basics Tutorial/Launcher: OnConnectedToMaster() was called by PUN");
-            // #Critical: The first we try to do is to join a potential existing room. If there is, good, else, we'll be called back with OnJoinRandomFailed()
-            PhotonNetwork.JoinRandomRoom();
+            // we don't want to do anything if we are not attempting to join a room.
+            // this case where isConnecting is false is typically when you lost or quit the game, when this level is loaded, OnConnectedToMaster will be called, in that case
+            // we don't want to do anything.
+            if (isConnecting)
+            {
+                // #Critical: The first we try to do is to join a potential existing room. If there is, good, else, we'll be called back with OnJoinRandomFailed()
+                PhotonNetwork.JoinRandomRoom();
+            }
         }
-
-
         public override void OnDisconnected(DisconnectCause cause)
         {
+            ToggleControlPanel(false);
             Debug.LogWarningFormat("PUN Basics Tutorial/Launcher: OnDisconnected() was called by PUN with reason {0}", cause);
         }
-
         public override void OnJoinRandomFailed(short returnCode, string message)
         {
             Debug.Log("PUN Basics Tutorial/Launcher:OnJoinRandomFailed() was called by PUN. No random room available, so we create one.\nCalling: PhotonNetwork.CreateRoom");
@@ -81,14 +124,19 @@ namespace BattleScripts
             // #Critical: we failed to join a random room, maybe none exists or they are all full. No worries, we create a new room.
             PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = maxPlayersPerRoom });
         }
-
         public override void OnJoinedRoom()
         {
             Debug.Log("PUN Basics Tutorial/Launcher: OnJoinedRoom() called by PUN. Now this client is in a room.");
+            // #Critical: We only load if we are the first player, else we rely on `PhotonNetwork.AutomaticallySyncScene` to sync our instance scene.
+            if (PhotonNetwork.CurrentRoom.PlayerCount == 1)
+            {
+                Debug.Log("Loading 'Room for 1' ");
+
+                // #Critical
+                // Load the Room Level.
+                PhotonNetwork.LoadLevel("Room for 1");
+            }
         }
-
         #endregion
-
-
     }
 }
