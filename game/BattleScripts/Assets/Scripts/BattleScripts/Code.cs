bc0fa7c28e5a79c7d3eff5a6e8f587032dfd3b1b
Brian Fong <brianwkfong@gmail.com>
2018-10-21 23:58:30 -0700

    Added Programmer and Code class'

---
+++ game/BattleScripts/Assets/Scripts/BattleScripts/Code.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace BattleScripts
+{
+	/// <summary>
+	/// Code which the Programmer could stock in their hand or program
+	/// - Display
+	/// - Action
+	/// - Execute()
+	/// </summary>
+	public class Code  {
+		public string Display;
+	}
+}
---
+++ game/BattleScripts/Assets/Scripts/BattleScripts/Programmer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BattleScripts
+{
+	public class Programmer : MonoBehaviour {
+
+		#region Private Fields
+		private List<Code> program;
+		private List<Code> hand;
+		
+		private byte foo;
+		private byte bar;
+		#endregion
+
+		#region Public Methods
+
+		#endregion
+
+		#region MonoBehaviour CallBacks
+		// Use this for initialization
+		void Start () {
+			
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			
+		}
+		#endregion
+	}
+}
