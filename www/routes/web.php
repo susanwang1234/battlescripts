6a15a6dbd43908ffb0d01451ada6da6aedd96a2c
eyy3 <emilie_yu@sfu.ca>
2018-10-22 22:41:48 -0700

    Update web.php; link tutorial and about page access

--- www/routes/web.php
+++ www/routes/web.php
@@ -18,3 +18,10 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/tutorial', function () {
+    return view('tutorial');
+});
+Route::get('/about', function () {
+    return view('about');
+});
