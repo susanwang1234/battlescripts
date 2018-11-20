cf073267dc7b2ddf03a4f8fb3d47881ebf2b619e
susan <susan_wang_3@sfu.ca>
2018-11-19 22:49:06 -0800

    made the transition smoother

--- www/resources/views/unity.blade.php
+++ www/resources/views/unity.blade.php
@@ -1,47 +1,48 @@
-@extends('layouts.app')
-
-@section('content')
-<head>
-	<title>Unity WebGL Player | BattleScripts</title>
-	<link rel="shortcut icon" href="{{ asset('game/TemplateData/favicon.ico')}}">
-	<link rel="stylesheet" href="{{asset('game/TemplateData/style.css')}}">
-	<script src="{{ URL::asset('game/TemplateData/UnityProgress.js')}}"></script>
-	<script src="{{ URL::asset('game/Build/UnityLoader.js')}}"></script>
-	<script>
-	  var gameInstance = UnityLoader.instantiate("gameContainer", "game/Build/Build.json", {onProgress: UnityProgress});
-	</script>
-	<link href="{{ asset('css/home.css') }}" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css?family=VT323" rel="stylesheet">
-</head>
-
-<body>
-{{--<div class="bg">
-	<div class="container">
-	    <div class="row justify-content-center">
-	        <div class="col-md-8">
-	        	<img src="{{URL::asset('/images/logo_text.png')}}" class="center-block" style="width:100%; height=auto">
-	        </div>
-	    </div>
-	</div>
-</div>--}}
-
-<div class="content-bg">
-<div class=categories>
-	<ul>
-	    <li><a href="/index.php">HOME</a></li>
-		<li><a href="/about">ABOUT</a></li>
-		<li><a href="/tutorial">TUTORIAL</a></li>
-		<li><a href="/start">GETTING STARTED</a></li>
-		<li><a id="activeplay" href="/unity">PLAY NOW</a></li>
-	</ul>
-</div>
-
 @if (empty(Auth::user()->email_verified_at))
 	<script type="text/javascript">
           window.location = "{{ url('/email/verify') }}";
     </script>
 @else
+	@extends('layouts.app')
+
+    @section('content')
+	<head>
+		<title>Unity WebGL Player | BattleScripts</title>
+		<link rel="shortcut icon" href="{{ asset('game/TemplateData/favicon.ico')}}">
+		<link rel="stylesheet" href="{{asset('game/TemplateData/style.css')}}">
+		<script src="{{ URL::asset('game/TemplateData/UnityProgress.js')}}"></script>
+		<script src="{{ URL::asset('game/Build/UnityLoader.js')}}"></script>
+		<script>
+		  var gameInstance = UnityLoader.instantiate("gameContainer", "game/Build/Build.json", {onProgress: UnityProgress});
+		</script>
+		<link href="{{ asset('css/home.css') }}" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=VT323" rel="stylesheet">
+	</head>
+
+	<body>
+	{{--<div class="bg">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-md-8">
+					<img src="{{URL::asset('/images/logo_text.png')}}" class="center-block" style="width:100%; height=auto">
+				</div>
+			</div>
+		</div>
+	</div>--}}
+
+	<div class="content-bg">
+	<div class=categories>
+		<ul>
+			<li><a href="/index.php">HOME</a></li>
+			<li><a href="/about">ABOUT</a></li>
+			<li><a href="/tutorial">TUTORIAL</a></li>
+			<li><a href="/start">GETTING STARTED</a></li>
+			<li><a id="activeplay" href="/unity">PLAY NOW</a></li>
+		</ul>
+	</div>
+
+
 	<div class="webgl-content" style="margin-top:5%; box-shadow: 0px 0px 50px lime;">
 		  <div id="gameContainer" style="width: 960px; height: 600px"></div>
 		  <div class="footer" style="margin-top:0">
@@ -50,16 +51,17 @@
 			<div class="title" style="color:white;">BattleScript || Team 404 &copy; 2018</div>
 		  </div>
 	</div>
+
+
+	<div class="footer" style="margin-top:50%">
+		<div id="footertext">Team 404 &copy; 2018</div>
+	</div>
+
+	</div>
+
+	</body>
+	@endsection
 @endif
-
-<div class="footer" style="margin-top:50%">
-	<div id="footertext">Team 404 &copy; 2018</div>
-</div>
-
-</div>
-
-</body>
-@endsection
 
 
 
