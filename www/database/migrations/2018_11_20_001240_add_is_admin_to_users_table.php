2de43821f55fea1aa812ff0a28db8ee97db18fe0
susan <susan_wang_3@sfu.ca>
2018-11-19 22:05:33 -0800

    changed migration table

--- www/database/migrations/2018_11_20_001240_add_is_admin_to_users_table.php
+++ www/database/migrations/2018_11_20_001240_add_is_admin_to_users_table.php
@@ -14,7 +14,7 @@ class AddIsAdminToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->tinyInteger('isAdmin');
+            $table->boolean('isAdmin')->default(false);
         });
     }
 
